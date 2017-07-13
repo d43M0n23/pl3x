1a30446b7458951e34a0432330846c5fffb7a72e
daemon-r7 <daemon@coremedia-solution.com>
2017-07-13 10:32:45 +0200

    update

--- metapl3x.sh
+++ metapl3x.sh
@@ -1,35 +1,40 @@
 #!/bin/bash
 
-# ffmpeg titel change
+# ffmpeg titel and comment change script
 #########################################################################################
-# script read all files and change title
-# remove "RARBG.COM - titel.mp4"
+# script read all mp4 files and change title and comment metadata 
+# like remove "RARBG.COM - titel.mp4"
 #########################################################################################
-
-#ffmpeg -i input.mp4 -c copy -metadata title="toller film" output.mp4
 #ffmpeg -i inputfile -metadata title="Movie Title" -metadata year="2010" outputfile
-#find /home/volker/Videos/ -name "*.mp4" -exec stat --printf="%n\n" {} \; > /tmp/mp4-liste.txt
 #exiftool IMG_0014.MOV >a.txt
 #ffmpeg -i input_video -f ffmetadata metadata.txt
 #exiftool bumsbuero.16.09.30.ashley.cumstar.de.mp4 | grep Title
 #############################################
-#ffmpeg -i input_video -f ffmetadata metadata.txt
-#exiftool bumsbuero.16.09.30.ashley.cumstar.de.mp4 | grep Title
-#DATA:
-#bumsbuero.16.09.30.ashley.cumstar.de.mp4
-#Title                           : RARBG.COM - BumsBuero.16.09.30.Ashley.Cumstar.GERMAN.XXX.1080p.MP4-KTR
-#Comment                         : RARBG.COM - BumsBuero.16.09.30.Ashley.Cumstar.GERMAN.XXX.1080p.MP4-KTR
+#METADATA:
+#Title
+#Comment
 #############################################
+#tools install
+if ! hash exiftool 2>/dev/null; then sudo apt-get update && apt-get upgrade -y; sudo apt-get install --yes exiftool ; fi
+if ! hash ffmpeg 2>/dev/null; then sudo apt-get install --yes ffmpeg ; fi
+
 
 for file in *;
 do
   find "$file" -type f -not -name ".*" | grep .mp4$ | while read file
   do
     pfad=$(readlink -f "$file") # kpl. Pfad
-    pfadOhneExt=${pfad%.*} # kpl. Pfad ohne Extension
-    mkvTag=$(basename "$pfadOhneExt") # Dateiname ohne MKV = Tag
-    echo "$mkvTag"
-#    mkvpropedit "$pfad" -s title="$mkvTag"
+	DirPath=$pfad
+	DirPath="$(dirname $DirPath)"
+	DirPath="$(basename $DirPath)"
+    	#pfadOhneExt=${pfad%.*} # kpl. Pfad ohne Extension
+    	#mp4Tag=$(basename "$pfadOhneExt") # Dateiname ohne MKV = Tag
+   	#echo "$mp4Tag"
+	echo "$DirPath"
+	newtag=$DirPath	
+	ffmpeg -i $file -metadata title="$newtag" -metadata comment="$newtag" -y $file
+	echo $file korrigiert.
+	#mkvpropedit "$pfad" -s title="$mkvTag"  #edit command titel etc
   done
 done
 
