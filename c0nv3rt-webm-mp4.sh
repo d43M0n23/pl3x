ffdac4b3124cb0e5fa58d8287686c54244bea16d
daemon-r7 <daemon@coremedia-solution.com>
2017-08-31 00:38:53 +0200

    update

--- c0nv3rt-webm-mp4.sh
+++ c0nv3rt-webm-mp4.sh
@@ -6,9 +6,6 @@
 ######################################################################
 
 ######################################################################
-# last modify: 31.08.2017
-# bug or anything: d43M0n23@3xpl0it.com
-######################################################################
 # TODO EXAMPLE:
 # fab_130628_0554_core.mp4
 # can to: cut -f 1 -d '_'
@@ -20,9 +17,9 @@
 # ffmpeg -i input-file.mp4 -c:v libvpx -crf 10 -b:v 1M -c:a libvorbis output-file.webm
 # webm 2 mp4
 # ffmpeg -fflags +genpts -i $0 -r 24 $1
-# last modify: 04.07.2017
+# last modify: 31.08.2017
 # bug or anything: d43M0n23@3xpl0it.com
-###################################################################################
+######################################################################
 
 #Variablen
 owntag=core
@@ -32,9 +29,6 @@ mkvtag=mkv
 # Tools install
 if ! hash exiftool 2>/dev/null; then sudo apt-get update && apt-get upgrade -y; sudo apt-get install --yes exiftool ; fi
 if ! hash ffmpeg 2>/dev/null; then sudo apt-get install --yes ffmpeg ; fi
-
-
-avconv -i $0 -c:v libx264  -crf 20 -c:a acc -strict experimental $1
 
 
 #for file in *.mp4 *.mkv (Testen)
