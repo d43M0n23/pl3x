75bbb095b24327bcc0b22368d1409a945f9f8c88
daemon-r7 <daemon@coremedia-solution.com>
2017-08-31 00:36:50 +0200

    update

--- c0nv3rt-webm-mp4.sh
+++ c0nv3rt-webm-mp4.sh
@@ -16,15 +16,44 @@
 # Global Command:
 # ffmpeg [global options] [input file options] -i input-file-name [output options] output-file-name
 # Variablen
+# mp4 2 webm
+# ffmpeg -i input-file.mp4 -c:v libvpx -crf 10 -b:v 1M -c:a libvorbis output-file.webm
+# webm 2 mp4
+# ffmpeg -fflags +genpts -i $0 -r 24 $1
+# last modify: 04.07.2017
+# bug or anything: d43M0n23@3xpl0it.com
+###################################################################################
 
+#Variablen
+owntag=core
+mp4tag=mp4
+mkvtag=mkv
 
-
-#mp4 2 webm
-#ffmpeg -i input-file.mp4 -c:v libvpx -crf 10 -b:v 1M -c:a libvorbis output-file.webm
-
-
-#webm 2 mp4
-#ffmpeg -fflags +genpts -i $0 -r 24 $1
+# Tools install
+if ! hash exiftool 2>/dev/null; then sudo apt-get update && apt-get upgrade -y; sudo apt-get install --yes exiftool ; fi
+if ! hash ffmpeg 2>/dev/null; then sudo apt-get install --yes ffmpeg ; fi
 
 
 avconv -i $0 -c:v libx264  -crf 20 -c:a acc -strict experimental $1
+
+
+#for file in *.mp4 *.mkv (Testen)
+# echo ${VALUE%.*}
+for file in *;
+do
+  find "$file" -type f -not -name ".*" | grep .webm$ | while read file
+  do
+    pfad=$(readlink -f "$file") # kpl. Pfad
+        DirPath=$pfad
+        DirPath="$(dirname $DirPath)"
+        DirPath="$(basename $DirPath)"
+        echo "$DirPath"
+ #       newtag=${DirPath%.XXX*}
+        newfile=${file%.*}
+	avconv -i $file -c:v libx264  -crf 20 -c:a acc -strict experimental ${newfile}.${mp4tag}
+        echo $file korrigiert.
+#       cp $file ${file}.bka # Absicherung
+#        rm $file
+  done
+done
+
